ef47353caf2e3c1c2883707d33d96f80816190c0
Raj Thombare <therajthombare@gmail.com>
2023-01-19 23:58:49 +0530

    add Swipe Drawer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import NotesForm from "../components/Notes/NotesForm";
 import Box from "@mui/material/Box";
+import SwipeDrawer from "../components/UI/SwipeDrawer";
 
 const Home = () => {
   return (
     <Box sx={{ display: "flex", width: "100%" }}>
       <Box sx={{ p: 3, width: "100%" }}>
+        <SwipeDrawer />
         <NotesForm />
       </Box>
     </Box>
