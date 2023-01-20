977b8a92d0a362b9eacc01bd04a81a7326e130ab
Raj Thombare <therajthombare@gmail.com>
2023-01-21 00:53:31 +0530

    add archive & trash pages with routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
+import AllNotes from "./pages/AllNotes";
+import Archive from "./pages/Archive";
+import Trash from "./pages/Trash";
 import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<AllNotes />} />
+      <Route path="/archive" element={<Archive />} />
+      <Route path="/trash" element={<Trash />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
