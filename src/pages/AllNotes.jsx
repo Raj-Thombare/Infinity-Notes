52fdac3317ce561173cde50c83b2656fa8b59dd4
Raj Thombare <therajthombare@gmail.com>
2023-01-21 23:47:20 +0530

    add empty notes background

--- src/pages/AllNotes.jsx
+++ src/pages/AllNotes.jsx
@@ -3,27 +3,28 @@ import { Box } from "@mui/material";
 import NotesForm from "../components/Notes/NotesForm";
 import SwipeDrawer from "../components/UI/SwipeDrawer";
 import NotesList from "../components/Notes/NotesList";
+import EmptyNotes from "../components/UI/EmptyNotes";
 
 const DrawerHeader = styled("div")(({ theme }) => ({
   ...theme.mixins.toolbar,
 }));
 
 const dummyNotes = [
-  {
-    id: 1,
-    title: "Note 1",
-    text: "This is my first note! This is my first note! This is my third note!",
-  },
-  {
-    id: 2,
-    title: "Note 2",
-    text: "This is my second note! This is my second note! This is my second note! This is my second note! This is my second note! This is my second note!",
-  },
-  {
-    id: 3,
-    title: "Note 3",
-    text: "This is my third note! This is my third note! This is my third note! This is my third note!",
-  },
+  // {
+  //   id: 1,
+  //   title: "Note 1",
+  //   text: "This is my first note! This is my first note! This is my third note!",
+  // },
+  // {
+  //   id: 2,
+  //   title: "Note 2",
+  //   text: "This is my second note! This is my second note! This is my second note! This is my second note! This is my second note! This is my second note!",
+  // },
+  // {
+  //   id: 3,
+  //   title: "Note 3",
+  //   text: "This is my third note! This is my third note! This is my third note! This is my third note!",
+  // },
 ];
 
 const Home = () => {
@@ -33,7 +34,11 @@ const Home = () => {
         <DrawerHeader />
         <SwipeDrawer />
         <NotesForm />
-        <NotesList notes={dummyNotes} />
+        {dummyNotes.length > 0 ? (
+          <NotesList notes={dummyNotes} />
+        ) : (
+          <EmptyNotes />
+        )}
       </Box>
     </Box>
   );
