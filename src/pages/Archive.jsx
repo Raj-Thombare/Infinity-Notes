24423b18c36c335aec4b601cec321f52526a800d
Raj Thombare <therajthombare@gmail.com>
2023-01-22 23:41:07 +0530

    add content on archive page

--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { styled } from "@mui/material/styles";
+import { Box } from "@mui/material";
+import SwipeDrawer from "../components/UI/SwipeDrawer";
+import NotesList from "../components/Notes/NotesList";
+import EmptyNotes from "../components/UI/EmptyNotes";
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  ...theme.mixins.toolbar,
+}));
 
 const Archive = () => {
-  return <div>Archive</div>;
+  return (
+    <Box sx={{ display: "flex", width: "100%" }}>
+      <Box sx={{ p: 3, width: "100%" }}>
+        <Box sx={{ marginLeft: "260px" }}>
+          <DrawerHeader />
+          <SwipeDrawer />
+          {false ? <NotesList /> : <EmptyNotes />}
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Archive;
